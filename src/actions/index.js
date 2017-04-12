0e05af3c307d192e60e1888b7ce3a31d2cd05e47
frandm182 <franjdm@gmail.com>
2017-04-12 17:16:03 +0200

    Create index.js

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios'; //TO MAKE GET REQUEST
+
+const API_KEY = '7f06be07815f9aa9d1528f0a5f0ef638';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}

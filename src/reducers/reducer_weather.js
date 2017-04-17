831931a4d3f74349f94f066209f378dab1dfc730
frandm182 <franjdm@gmail.com>
2017-04-17 11:58:22 +0200

    Add files via upload

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,13 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+	console.log('Action received', action);
+	switch (action.type) {
+		case FETCH_WEATHER:
+			//console.log('Entramos en Fetch',  action.payload.data);
+			//console.log('Entramos en state',  state);
+			//console.log ([ action.payload.data, ...state ]);
+			return [ action.payload.data, ...state ];
+	}
 	return state;
 }
